fe268636f3647692fa49c6765ee22d7cf87d67cf
Mourad Majdouli <129498509+Mourad261103@users.noreply.github.com>
2024-04-17 14:52:42 +0200

    Deck completed :)

---
+++ src/main/java/dev/starless/briscola/api/LogicGame.java
@@ -0,0 +1,31 @@
+package dev.starless.briscola.api;
+
+import dev.starless.briscola.DeckImpl;
+
+import java.util.Scanner;
+
+public class LogicGame {
+public void Game()
+{
+    DeckImpl mazzo = new DeckImpl();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Inserisci il nome p1: ");
+    String input = scanner.nextLine();
+    Player p1=new Player(input);
+    input = scanner.nextLine();
+    Player p2=new Player(input);
+    scanner.close();
+    while (true)
+    {
+
+    }
+
+}
+
+    public static void main(String[] args) {
+     LogicGame partita= new LogicGame();
+     partita.Game();
+
+    }
+
+}
