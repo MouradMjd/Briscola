ec6d36faa8a4a01314a6ede7fdb5d4f775b02949
StarlessDev <contact@starless.dev>
2024-04-21 17:36:50 +0200

    Written method to check for a trump card

--- src/main/java/us/teamronda/briscola/DeckImpl.java
+++ src/main/java/us/teamronda/briscola/DeckImpl.java
@@ -9,15 +9,28 @@ import java.util.Collections;
 
 public class DeckImpl extends AbstractDeck {
 
+    private Seed trumpSeed;
+
     @Override
     public void create() {
+        // Remove any cards from the previous game
         cards.clear();
 
+        // Populate the deck
         for (CardType type : CardType.values()) {
             for (Seed seed : Seed.values()) {
                 cards.add(new Card(type, seed));
             }
         }
+
+        /*
+        We take the first card,
+        store its seed in a variable
+        and append it at the end of the deck
+         */
+        Card trumpCard = popCard();
+        trumpSeed = trumpCard.seed();
+        cards.addLast(trumpCard);
     }
 
     @Override
@@ -25,20 +38,24 @@ public class DeckImpl extends AbstractDeck {
         Collections.shuffle(cards);
     }
 
-    public Card takeBriscola() {
-        if (isEmpty()) {
-            return null;
-        }
-        return cards.getLast();
-    }
-
     @Override
     public Card popCard() {
         if (isEmpty()) {
             return null;
         }
+
         Card card = cards.getFirst();
         cards.remove(card);
         return card;
+    }
+
+    /**
+     * Checks if a card has the same seed of the
+     * trump card.
+     *
+     * @param card Card object to check
+     */
+    public boolean hasTrumpSeed(Card card) {
+        return trumpSeed != null && trumpSeed.equals(card.seed());
     }
 }
