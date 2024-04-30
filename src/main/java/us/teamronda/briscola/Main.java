5fa6867628646d759b61ba8702704882d916ec26
Mourad Majdouli <129498509+Mourad261103@users.noreply.github.com>
2024-04-30 22:30:03 +0200

    start funge 🔥

--- src/main/java/us/teamronda/briscola/Main.java
+++ src/main/java/us/teamronda/briscola/Main.java
@@ -2,12 +2,10 @@ package us.teamronda.briscola;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 import static javafx.application.Application.launch;
 
@@ -16,15 +14,14 @@ public class Main extends Application {
         LogicGame game = new LogicGame();
         game.startGameLoop();
     }
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent view = FXMLLoader.load(getResource("start.fxml"));
-        Scene scene = new Scene(view);
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/start.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
