3e7558bfbb7586ae3c1dc6c25c35ecf653598c87
Mourad Majdouli <129498509+Mourad261103@users.noreply.github.com>
2024-05-07 00:58:28 +0200

    work on css part

--- src/main/java/us/teamronda/briscola/Main.java
+++ src/main/java/us/teamronda/briscola/Main.java
@@ -6,10 +6,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("gui/fxmls/start.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
