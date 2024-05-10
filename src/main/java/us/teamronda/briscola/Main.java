3d538452a3b0c93cbbb9b23877f6d334e3f95dbf
StarlessDev <contact@starless.dev>
2024-05-10 08:35:31 +0200

    Using Application#stop properly to do stuff on shutdown

--- src/main/java/us/teamronda/briscola/Main.java
+++ src/main/java/us/teamronda/briscola/Main.java
@@ -13,19 +13,21 @@ import java.util.Objects;
 
 public class Main extends Application {
 
+    @Override
     public void start(Stage stage) throws IOException {
         // Load the main FXML file
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("gui/fxmls/start.fxml"));
         // Set the program icon
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/assets/images/icon.png"))));
-        stage.setOnCloseRequest(event -> {
-            TimerUtils.terminate(); // Terminate all timer tasks
-            Platform.exit(); // Shutdown gracefully using JavaFX
-        });
         stage.setResizable(false);
         stage.setTitle("Briscola v6.9");
         stage.setScene(new Scene(fxmlLoader.load()));
         stage.show();
+    }
+
+    @Override
+    public void stop() {
+        TimerUtils.terminate(); // Terminate all timer tasks
     }
 
     public static void main(String[] args) {
